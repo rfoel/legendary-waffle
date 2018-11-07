ca47c7db7db08024bd32f6b251bbf4fd265abe37
Rafael Franco <rafaelfr@outlook.com>
2018-11-07 10:27:31 -0200

    Updates

---
+++ precache-manifest.b3f404d39080058ffacdc84b9c00c6ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0558a81b08d7fe7215102a09a3ad3f1",
+    "url": "./static/media/waffle.a0558a81.svg"
+  },
+  {
+    "revision": "baf0228cd23daa5d9ca2c4dd821e664e",
+    "url": "./static/media/search.baf0228c.svg"
+  },
+  {
+    "revision": "ab587affd3ffe0829610a738b559c689",
+    "url": "./static/media/email.ab587aff.svg"
+  },
+  {
+    "revision": "096ec7034a5eae6a8bdc9d914681ef54",
+    "url": "./static/media/calendar.096ec703.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1db0c4556b5ea9f7374a",
+    "url": "./static/js/main.1db0c455.chunk.js"
+  },
+  {
+    "revision": "f884d3a06aff79823c74",
+    "url": "./static/js/1.f884d3a0.chunk.js"
+  },
+  {
+    "revision": "75ce147c888d70f96850e7da5895a5f8",
+    "url": "./index.html"
+  }
+];
